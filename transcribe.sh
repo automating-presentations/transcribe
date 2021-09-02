328953d7dce6b7ec4d17cc378c4500dd87536a45
Hirofumi Kojima <hkojima@redhat.com>
2021-09-02 11:57:58 +0900

    updated

--- transcribe.sh
+++ transcribe.sh
@@ -26,36 +26,33 @@ print_usage ()
 	echo "	$(basename $0) uses Amazon S3 and Amazon Transcribe, Speech-to-Text service."
 	echo "	$(basename $0) requires the following commands, aws s3, aws transcribe, ffmpeg, python3."
 	echo "Usage:"
-	echo "	$(basename $0) [option] -i <sound or movie file> -bucket <bucket name> -o <output file name>"
+	echo "	$(basename $0) [option] -i <sound or movie file> -o <output file name>"
 	echo "Options:"
 	echo "	-h, --help			print this message."
 	echo "	-lang <language code>		specify the language code. (default language code is \"ja-JP\")"
 	echo ""
 	echo "Example1: The following command transcribes Japanese sound or movie file and creates \"test-output.{json,txt}\"."
-	echo "	This S3 Bucket, \"testid-bucket00001\", is created temporarily for the purpose of transcribe, and deleted after transcribe."
 	echo ""
-	echo "	$(basename $0) -i input-ja.mp4 -bucket testid-bucket00001 -o test-output"
+	echo "	$(basename $0) -i input-ja.mp4 -o test-output"
 	echo ""
 	echo "Example2: The following command transcribes English sound or movie file and creates \"test-output.{json,txt}\"."
 	echo ""
-	echo "	$(basename $0) -lang en-US -i input-en.mp4 -bucket testid-bucket00001 -o test-output"
+	echo "	$(basename $0) -lang en-US -i input-en.mp4 -o test-output"
 	exit
 }
 
 
 # Random String
-RS=$(cat /dev/urandom |base64 |tr -cd "a-zA-Z0-9" |fold -w 32 |head -n 1)
+RS=$(cat /dev/urandom |base64 |tr -cd "a-z0-9" |fold -w 32 |head -n 1)
 
 
-INPUT_FLAG=0; BUCKET_FLAG=0; OUTPUT_FLAG=0
+INPUT_FLAG=0; OUTPUT_FLAG=0
 while [ $# -gt 0 ]
 do
 	if [ "$1" == "-h" -o "$1" == "--help" ]; then
 		print_usage
 	elif [ "$1" == "-i" ]; then
 		INPUT_FLAG=1; shift; INPUT_FILE="$1"; shift
-	elif [ "$1" == "-bucket" ]; then
-		BUCKET_FLAG=1; shift; BUCKET_NAME="$1"; shift
 	elif [ "$1" == "-o" ]; then
 		OUTPUT_FLAG=1; shift; OUTPUT_NAME="$1"; shift
 	elif [ "$1" == "-lang"  ]; then
@@ -68,10 +65,6 @@ if [ $INPUT_FLAG -eq 0 ]; then
 	echo "Please specify input file, -i <sound or movie file>."
 	echo "Please check '$(basename $0) -h' or '$(basename $0) --help'."
 	exit
-elif [ $BUCKET_FLAG -eq 0 ]; then
-	echo "Please specify AWS S3 Bucket name, -bucket <bucket name>."
-        echo "Please check '$(basename $0) -h' or '$(basename $0) --help'."
-        exit
 elif [ $OUTPUT_FLAG -eq 0 ]; then
 	echo "Please specify output file name, -o <output file name>."
         echo "Please check '$(basename $0) -h' or '$(basename $0) --help'."
@@ -89,6 +82,7 @@ fi
 rm -f tmp-$RS.txt
 
 
+BUCKET_NAME=transcribe-$RS
 aws s3api create-bucket --bucket "$BUCKET_NAME" --region "$REGION" --create-bucket-configuration LocationConstraint="$REGION" 2> tmp-$RS.txt
 if [ -s tmp-$RS.txt ]; then
 	cat tmp-$RS.txt
